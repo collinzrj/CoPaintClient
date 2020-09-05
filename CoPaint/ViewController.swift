a9d83d53c0a9e949a5674d0eec536436645bbfad
collin <collinzrj@outlook.com>
2020-09-05 11:47:39 +0800

    Initial Commit

---
+++ CoPaint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoPaint
+//
+//  Created by 张睿杰 on 2020/9/5.
+//  Copyright © 2020 张睿杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
